c5dd88d6b7f171ad3cf34d330aa719914e83b247
muhd-nish4d <mn0265616@gmail.com>
2025-05-06 11:07:04 +0530

    Loading fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2656972132"
+    serviceWorkerVersion: "872042101"
   }
 });
